cea814d8c3148a746afe9da9211f0e0b622ac95b
Hozenyth <hozenyth.andrade@gmail.com>
2022-02-12 11:24:41 -0300

    Utilizando DataAnnotations como validação ao inserir dados através do método post

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmesAPI.Models
 {
     public class Filme
-    {
+    {   
+        [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
         public string Diretor { get; set; }
+        [Required(ErrorMessage = "O campo genero é obrigatório")]
         public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
         public int Duracao { get; set; }
     }
 }
